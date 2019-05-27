d053c1d3150f96d72fded9bda32d3f5f74e31fe9
klindyuk <klindyuk@gmail.com>
2019-05-27 22:07:26 +0300

    ...

--- task1.php
+++ task1.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+	
+
 <?php
 abstract class Good {
 	private $sku;
@@ -13,11 +24,29 @@ abstract class Good {
 	public function setName($name) {
 		$this->name = $name;
 	}
-	public function getSku() {
+	public function getName() {
 		return $this->name;
 	}
 	public function getPrice() {
 		return $this->price;
 	}
-	abstract function setPrice();
+	abstract function setPrice($price);
 }
+
+class singleBook extends Good {
+	public function __construct($sku, $name, $price) {
+		parent::setSku($sku);
+		$this->setName($name);
+		$this->setPrice($price);
+		echo "Новый штучный товар: артикул - ".$this->getSku().", название - ".$this->getName().", цена - ".$this->getPrice();
+	}
+	public function setPrice($price) {
+		$this->price = $price;
+	}
+}
+
+$b1 = new singleBook(1, "New book", 100);
+
+?>
+</body>
+</html>
